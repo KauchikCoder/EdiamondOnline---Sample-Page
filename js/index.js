8359c9c8ddfe6bfc40d02d4f8cbf9bfdc09a5b53
kauchik <kauchik@gmail.com>
2022-10-15 22:49:44 +0530

    fifth commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
-  $(".menu-toggle").click(function () {
-    $("nav").toggleClass("active");
-  });
+    $(".menu-toggle").click(function () {
+        $("nav").toggleClass("active");
+    });
+});
+
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+let searchBtn = document.querySelector(".bx-search");
+
+closeBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("open");
+    menuBtnChange(); //calling the function(optional)
 });
